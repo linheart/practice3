b70ee1c13961826eb8b888342e34918905e2d2f3
Linheart <leshamihaylenko@bk.ru>
2024-05-28 11:27:10 +0700

    3d_ex

---
+++ 3rd_ex.cpp
@@ -0,0 +1,33 @@
+/* У Васи был набор целых чисел. Коля решил пошутить над Васей
+и добавил в набор буквы. Необходимо найти буквы в наборе и удалить их.
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void remove_letters(vector<string> &arr);
+
+int main() {
+  vector<string> arr = {"123a32", "132143", "d1123"};
+
+  remove_letters(arr);
+
+  for (auto it : arr) {
+    cout << it << ' ';
+  }
+  cout << endl;
+
+  return 0;
+}
+
+void remove_letters(vector<string> &arr) {
+  transform(arr.begin(), arr.end(), arr.begin(), [](string &str) {
+    str.erase(
+        remove_if(str.begin(), str.end(), [](char c) { return isalpha(c); }),
+        str.end());
+    return str;
+  });
+}
