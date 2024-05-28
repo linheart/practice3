5d5ac3b2dbedfa0111ff5a6424fb9efafcbea23c
Linheart <leshamihaylenko@bk.ru>
2024-05-28 09:45:53 +0700

    1st-ex

---
+++ 1st_ex.cpp
@@ -0,0 +1,36 @@
+/* Есть группа учеников, каждый из которых пришел сдавать лабораторную
+работу. Каждая лабораторная работа имеет сложность, которая обозначается
+числом от 1 до 5.
+Нужно составить такую очередь, чтобы преподаватель смог принять максимальное
+количество лабораторных работ за определенное время.
+*/
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+using Students = vector<pair<string, pair<string, int>>>;
+
+int main() {
+    Students students = {
+        {"Alexei", {"lab7", 1}},
+        {"Ivan", {"lab6", 5}},
+        {"Ilya", {"lab3", 4}},
+        {"Semen", {"lab4", 3}},
+        {"Katya", {"lab2", 4}},
+        {"Sasha", {"lab5", 3}}
+    };
+
+    sort(students.begin(), students.end(), [](auto& a, auto& b) {
+        return a.second.second < b.second.second;
+    });
+
+    for (auto& student : students) {
+        cout << student.first << ": " << student.second.first << "(" << student.second.second << ")" << endl;
+    }
+
+    return 0;
+}
+
