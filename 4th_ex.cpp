9a39c7656d6627d3bb5a824ffd8fd0e66b290f0b
Linheart <leshamihaylenko@bk.ru>
2024-05-28 11:43:28 +0700

    4th_ex

---
+++ 4th_ex.cpp
@@ -0,0 +1,36 @@
+/*Есть список студентов, но некоторые из которых решили уйти в 
+академический отпуск после сессии. Необходимо удалить этих 
+студентов из списка и вывести обновленный список.
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void remove_students(vector<string> &, vector<string> &);
+
+int main() {
+  vector<string> students = {"Anna", "Ivan", "Maria", "Petr", "Katya"};
+  vector<string> students_on_leave = {"Ivan", "Petr"};
+
+  remove_students(students, students_on_leave);
+
+  for (auto student : students) {
+    cout << student << endl;
+  }
+
+  return 0;
+}
+
+void remove_students(vector<string> &students,
+                     vector<string> &students_on_leave) {
+  students.erase(remove_if(students.begin(), students.end(),
+                           [&students_on_leave](string &student) {
+                             return find(students_on_leave.begin(),
+                                         students_on_leave.end(),
+                                         student) != students_on_leave.end();
+                           }),
+                 students.end());
+}
